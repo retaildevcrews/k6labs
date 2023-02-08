35018911080e06c2694388854aa3c792c0076717
Jorge Luna <lunajorge@microsoft.com>
2023-02-08 15:47:55 -0600

    making ngsa script

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,11 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('https://test.k6.io');
-  sleep(1);
+  let base_url='https://ngsa-cosmos-westus3-dev.austinrdc.dev';
+  http.get(base_url+'/api/actors/nm0000284');
+  sleep(1)
+  http.get(base_url+'/api/movies/tt1272878');
+  sleep(1)
+  http.get(base_url+'/api/movies?genre=Animation');
+  sleep(1)
 }
