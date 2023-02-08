21ea869739e39a87834110a6de4e9b9bee7c413a
Jorge Luna <lunajorge@microsoft.com>
2023-02-08 17:38:54 -0600

    updating lr2k6

--- python/lr2k6.py
+++ python/lr2k6.py
@@ -3,18 +3,28 @@ import json
 script_path= '/Users/jorgeluna/work/k6labs/scripts/benchmark.json'
 lr_script= open(script_path)
 
+def lrverb_2_k6verb(lrverb):
+    verb=lrverb.lower()
+    if verb=='delete':
+        return 'del'
+    else:
+        return verb
+
+
+
 script_contents = json.load(lr_script)
 print('import http from \'k6/http\';')
 print('import { sleep } from \'k6\';')
 print('')
 print('export default function () {')
 print('\tlet maxSleep=5;')
+print('\tlet res')
 print('\tlet baseURL=\'https://ngsa-cosmos-westus3-dev.austinrdc.dev\';')
 for request in script_contents['requests']:
     httpRequestString='baseURL+\''+request['path']+'\''
+    httpVerb='get'
     if 'verb' in request:
-        print('\thttp.'+request['verb'].lower()+'('+httpRequestString+');')
-    else:
-        print('\thttp.get('+httpRequestString+');')
+        httpVerb=lrverb_2_k6verb(request['verb'])
+    print('\tres=http.'+httpVerb+'('+httpRequestString+');')
     print('\tsleep(Math.random() * maxSleep)')
 print('}')
