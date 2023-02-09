8bd54fb627fcbf7e70d3c41024437fbd4868d7dd
Jorge Luna <lunajorge@microsoft.com>
2023-02-09 09:38:06 -0600

    updating repo from code in fork in order to transfer ownership

--- python/lr2k6.py
+++ python/lr2k6.py
@@ -1,7 +1,10 @@
+"""Module providingFunction printing python version."""
 import json
 import argparse
 
+
 def get_args():
+    """Get and parse arguments for CLI"""
     ## Argument Parse
     parser = argparse.ArgumentParser(
         description="Convert LoadRunner script to k6 script"
@@ -29,33 +32,39 @@ def get_args():
     )
     return parser.parse_args()
 
+
 def lrverb_2_k6verb(lrverb):
+    """Translates verbs from LodeRunner files to k6 verbs"""
     verb = lrverb.lower()
     if verb == "delete":
         return "del"
-    else:
-        return verb
+    return verb
 
-def print_validator(validation_object):
+def validation_2_check(validation_object):
+    """Generates check for k6 script from Loderunner Validation Object"""
     check_name=list(validation_object.keys())[0]
-    if 'statusCode' in validation_object:
-         print(
-    f"""    check(res, {{
-        {check_name}:(r) =>
-        r.status === {validation_object['statusCode']},
-        }});""")
-    elif 'jsonArray' in validation_object:
-         print(
-    f"""    check(res, {{
-        {check_name}:(r) =>
-        r.json().length === {validation_object['jsonArray']['count']},
-        }});""")   
-    elif 'jsonObject' in validation_object:
-         print(
-    f"""    check(res, {{
-        {check_name}:(r) =>
-        JSON.stringify(r.json()) === JSON.stringify({json.dumps(validation_object['jsonObject'])}),
-        }});""")   
+    contains_supported_validation=False
+    validator="""    check(res, {
+        """
+    for validation in validation_object:
+        check_name=validation
+        print(f"    //LR script contains validator: '{check_name}'")
+        if check_name=='statusCode':
+            contains_supported_validation=True
+            validator=validator+f"""    "{check_name}":(r) => r.status === {validation_object['statusCode']},
+                """
+        elif check_name=='jsonArray':
+            contains_supported_validation=True
+            validator=validator+f"""    "{check_name}":(r) => r.json().length === {validation_object['jsonArray']['count']},
+            """  
+        elif check_name=='jsonObject':
+            contains_supported_validation=True
+            validator=validator+f"""    "{check_name}":(r) => JSON.stringify(r.json()) === JSON.stringify({json.dumps(validation_object['jsonObject'])}),
+            """
+    validator=validator+"""    });"""
+    if contains_supported_validation:
+        return validator
+    return "    //No supported validators existed for this request."
 
 if __name__ == "__main__":
     args=get_args()
@@ -63,7 +72,7 @@ if __name__ == "__main__":
     sleep_ms = args.sleep
     base_url = args.base_url
     ## `with` statement automatically closes the file
-    with open(lr_json_path) as lr_script:
+    with open(lr_json_path, encoding="utf8") as lr_script:
         script_contents = json.load(lr_script)
         print(
 """import http from 'k6/http';
@@ -74,7 +83,6 @@ export default function () {"""
         )
 
         print(
-            
     f"""
     let maxSleep={sleep_ms};
     let res
@@ -83,12 +91,12 @@ export default function () {"""
         )
         for request in script_contents["requests"]:
             httpRequestString = "baseURL+'" + request["path"] + "'"
-            httpVerb = "get"
+            HTTP_VERB = "get"
             if "verb" in request:
-                httpVerb = lrverb_2_k6verb(request["verb"])
-            print("    res=http." + httpVerb + "(" + httpRequestString + ");")
+                HTTP_VERB = lrverb_2_k6verb(request["verb"])
+            print("    res=http." + HTTP_VERB + "(" + httpRequestString + ");")
             if "validation" in request:
-                print_validator(request["validation"])
+                print(validation_2_check(request["validation"]))
             print("    sleep(Math.random() * maxSleep)")
             print("")
         print("}")
