d7d010478eb7a52443b3efe1683918ac5ae6a72e
gortegaMS <70150605+gortegaMS@users.noreply.github.com>
2023-05-15 18:24:33 +0000

    Updated on-create

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -36,6 +36,10 @@ k3d registry create registry.localhost --port 5000
 # Connect to local registry
 docker network connect k3d k3d-registry.localhost
 
+# install httpie
+sudo apt install httpie
+
 echo "on-create completed" > $HOME/status
+
 
 status "end"
